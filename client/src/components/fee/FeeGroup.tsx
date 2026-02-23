a6f4c7ad14087485a7a101106faae223eac6a2fc
joy-narayon-roy <joyroy261216@gmail.com>
2026-02-23 12:29:09 +0600

    Update fees design.

--- client/src/components/fee/FeeGroup.tsx
+++ client/src/components/fee/FeeGroup.tsx
@@ -36,15 +36,17 @@ export default function GroupFee(params: GroupFeeParams) {
 
     return (
         <>
-            <div className="mt-2 rounded-md border-2 border-gray-300 overflow-hidden">
-                <h1 className="px-3 py-2 text-center bg-gray-100 text-lg">{months[info.month - 1]}-{info.year}</h1>
-                <div className="px-2 py-2 flex flex-col gap-3">
-                    {info.fees.sort((a,b)=>statusSortPoints[a.status]-statusSortPoints[b.status]).map(f => <FeeGroupRow key={f.id} fee={f} />)}
-                </div>
-                <div className="px-5 py-1 mt-1 flex gap-5 bg-gray-100 justify-end text-sm text-gray-600">
-                    <span>Collected : {summery.collected}</span>
-                    <span>Discount: {summery.discount}</span>
-                    <span>Total: {summery.total}</span>
+            <div className="mt-2 overflow-hidden ">
+                <h1 className="px-3 py-2 text-center rounded-t-md text-lg bg-linear-to-r from-indigo-600 to-purple-600 text-white">{months[info.month - 1]}-{info.year}</h1>
+                <div className="border-x-2 border-b-2 rounded-b-md overflow-hidden border-gray-200">
+                    <div className="p-2 flex flex-col gap-3">
+                        {info.fees.sort((a, b) => statusSortPoints[a.status] - statusSortPoints[b.status]).map(f => <FeeGroupRow key={f.id} fee={f} />)}
+                    </div>
+                    <div className="px-5 py-2 mt-1 flex gap-5 bg-gray-100 justify-end text-sm text-gray-600">
+                        <span>Collected : {summery.collected}</span>
+                        <span>Discount: {summery.discount}</span>
+                        <span>Total: {summery.total}</span>
+                    </div>
                 </div>
             </div>
         </>
