a9543e8bac15b00d7f2ca3885873c53a91461865
joy-narayon-roy <joyroy261216@gmail.com>
2026-07-06 00:15:23 +0600

    firebase hosting added

--- client/src/tools/apiRequest.ts
+++ client/src/tools/apiRequest.ts
@@ -4,5 +4,8 @@ import getBaseURL from "./getBaseURL";
 const apiRequest = axios.create({
   baseURL: getBaseURL(import.meta.env.VITE_API_PORT) + "/api",
 });
+// const apiRequest = axios.create({
+//   baseURL: "http://localhost:2000/api",
+// });
 
 export default apiRequest;
